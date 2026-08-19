9d27e498083e2f3e86cd8f66f05d0a87c75f7177
bapandev <adhikaryarts@gmail.com>
2026-08-19 23:20:52 +0530

    Update master.js

--- master.js
+++ master.js
@@ -1,1 +1,50 @@
+(function () {
+    'use strict';
 
+    const host = location.hostname;
+
+    const scripts = {
+        'web.whatsapp.com': 'whatsapp.js',
+        'aroapp.in': 'aroapp.js',
+
+        // Future websites
+        // 'example.com': 'example.js',
+    };
+
+    const file = scripts[host];
+
+    if (!file) {
+        return;
+    }
+
+    const url =
+        'https://raw.githubusercontent.com/bapandev/adhikaryarts/main/'
+        + file
+        + '?t='
+        + Date.now();
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP ${response.status}`);
+            }
+
+            return response.text();
+        })
+        .then(code => {
+            new Function(code)();
+
+            console.log(
+                '[AdhikaryArts] Loaded:',
+                file
+            );
+        })
+        .catch(error => {
+            console.error(
+                '[AdhikaryArts] Failed:',
+                file,
+                error
+            );
+        });
+
+})();
